00c34ed3295a2e33a41010e2afa74f24dde871eb
Melodiean <melodynsita@gmail.com>
2021-07-25 02:20:26 +0300

    Refactor

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ mongoose.connect(
   }
 );
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
